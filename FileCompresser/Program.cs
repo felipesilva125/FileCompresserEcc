b7c36e33e45bd85efacf3bcf200b3a67f54ef6ce
Felipe Silva <felipe.silva@ska.com.br>
2020-10-04 16:57:35 -0300

    Adjust main class

--- FileCompresser/Program.cs
+++ FileCompresser/Program.cs
@@ -6,22 +6,22 @@ namespace FileCompresser
     {
         public static void Main()
         {
-            //try
-            //{
-            var fileName = RequestData.RequestFileName();
-            var codificationType = RequestData.RequestCodificationType();
+            try
+            {
+                var fileName = RequestData.RequestFileName();
+                var codificationType = RequestData.RequestCodificationType();
 
-            var encoder = EncoderFactory.Create(codificationType);
+                var encoder = EncoderFactory.Create(codificationType);
 
-            compressFile(fileName, encoder);
-            //eccFile(fileName);                // tem que ter algo pra gerar ecc
-            decompressFile(fileName, encoder);
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine($"Error: {e.Message}");
-            //    Environment.Exit(0);
-            //}
+                compressFile(fileName, encoder);
+                //eccFile(fileName);                // tem que ter algo pra gerar ecc
+                decompressFile(fileName, encoder);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+                Environment.Exit(0);
+            }
 
             Console.WriteLine("Compressing and decompressing made successfully!");
         }
@@ -30,16 +30,12 @@ namespace FileCompresser
         {
             var fileContent = FileController.ReadFileContentString(fileName, FileController.READING_EXTENSION);
             encoder.Encode(fileContent, fileName);
-
-            //FileController.WriteFileContent(fileName, FileController.COMPRESSING_EXTENSION, fileContent);
         }
 
         private static void decompressFile(string fileName, IEncoder encoder)
         {
             var fileContent = FileController.ReadFileContent(fileName, FileController.ECC_EXTENSION);
-            encoder.Decode(fileContent, fileName);
-
-            //FileController.WriteFileContent(fileName, FileController.DECOMPRESSING_EXTENSION, fileContent);
+            encoder.Decode(fileContent, fileName);            
         }
     }
 }
